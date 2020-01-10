ce70a5cd246bfb334a65c818e23868adc8e3322f
poisoned-monkey <alcuzmichev@yandex.ru>
2020-01-11 01:31:02 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/main.cpp" "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/cmake-build-debug/CMakeFiles/main.dir/main.cpp.o"
+  "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/pentagon.cpp" "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/cmake-build-debug/CMakeFiles/main.dir/pentagon.cpp.o"
+  "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/rhombus.cpp" "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/cmake-build-debug/CMakeFiles/main.dir/rhombus.cpp.o"
+  "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/trapeze.cpp" "/Users/nataliazavodnova/Documents/MAI_study/second_year/oop_exercise_08/oop_exercise_08/cmake-build-debug/CMakeFiles/main.dir/trapeze.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
